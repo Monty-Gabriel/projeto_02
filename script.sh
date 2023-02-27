118adf3c05daf7b90e905a9dd20d383e270ceba5
Monty <tecnisoft01@gmail.com>
2023-02-27 19:29:55 -0300

    Upload Files 2

--- script.sh
+++ script.sh
@@ -7,11 +7,8 @@ apt install -y unzip
 apt install -y wget
 apt install -y htop
 apt install -y net-tools
-service restart net-tools
 apt install -y nmap
-service restart nmap
 apt install -y telnet
-service telnet restart
 apt install -y nginx
 service nginx restart
-sudo usseradd -m -s /bin/bash/ monty
+usseradd -m monty
