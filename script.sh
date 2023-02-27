fb65b54e8d505f4f37a6b87528532f2894c0a435
Monty <tecnisoft01@gmail.com>
2023-02-27 19:42:29 -0300

    Upload Files 3

--- script.sh
+++ script.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-apt update
-apt install -y vim
-apt install -y curl
-apt install -y unzip
-apt install -y wget
-apt install -y htop
-apt install -y net-tools
-apt install -y nmap
-apt install -y telnet
-apt install -y nginx
-service nginx restart
-usseradd -m monty
+sudo apt-get update
+sudo apt-get install -y vim curl telnet unzip wget net-tools htop nmap
+sudo service nginx restart
+#Criar usuário
+sudo useradd -m -s /bin/bash monty
+
+# Senha do usuário
+echo "monty:senha123" | sudo chpasswd
